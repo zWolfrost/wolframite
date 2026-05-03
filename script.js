548cd4d5e3c99d0a1f0c7ad6b9cf4626ed29dba7
Luca Russo <zwolfrost@gmail.com>
2026-05-03 23:45:22 +0200

    Reapply "+"
    This reverts commit 9091ec4ac3c1ccdc59764b07abbd3921678bc02e.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function animateTextDVD(el, speed) {
 	let x = 0, y = 0;
-	let dx = speed, dy = speed;
+	let dp = 0.5;
+	let dx = dp, dy = dp;
 	let colorIndex = 0;
 	let textIsLocked = true
+	let last = performance.now();
 
 	function setRandomRainbowColor(el) {
 		const colors = ["#f33", "#f93", "#ff5", "#6f6", "#6ff", "#66f", "#f6f"];
@@ -18,13 +20,17 @@ function animateTextDVD(el, speed) {
 	}
 
 	function bounceText() {
+		let now = performance.now();
+		let delta = now - last;
+		last = now;
+
 		let boxRect = el.parentElement.getBoundingClientRect();
 		let textRect = el.getBoundingClientRect();
 		let diffX = boxRect.width - textRect.width;
 		let diffY = boxRect.height - textRect.height;
 
-		x += dx;
-		y += dy;
+		x += dx * delta * speed;
+		y += dy * delta * speed;
 
 		if (textIsLocked) {
 			if (x <= 0 || x >= diffX) {
@@ -40,9 +46,9 @@ function animateTextDVD(el, speed) {
 				alert("DVD Logo Hit the Corner! 🎉");
 			}
 
-			if (x > diffX + speed || y > diffY + speed) {
+			if (x > diffX + dp * delta * speed || y > diffY + dp * delta * speed) {
 				x = y = 0
-				dx = dy = speed
+				dx = dy = dp
 			}
 		}
 
@@ -60,7 +66,7 @@ function animateTextDVD(el, speed) {
 	});
 }
 
-animateTextDVD(document.getElementById("bouncing-dvd"), 0.4);
+animateTextDVD(document.getElementById("bouncing-dvd"), 0.1);
 
 
 
